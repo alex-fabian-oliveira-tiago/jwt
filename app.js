1b069558e8c49465cfd25b7439ab56638053d29c
alex-fabian-oliveira-tiago <aftiago@gmail.com>
2022-01-13 17:12:29 -0300

    Última alteração: 13/01/2022 17:11 Sistema finalizado!

--- app.js
+++ app.js
@@ -20,26 +20,57 @@ app.get('/', (req, res) => {
 })
 
 /* Privates Routes */
+app.get('/private/:id', checkToken, async(req, res) => {
+    const id = req.params.id
+
+    // Check if User exists
+    const user = await User.findById(id, '-password')
+
+    if (!user) {
+        return res.status(404).json({ message: 'Usuário não cadastrado!' })
+    }
+
+    return res.status(200).json({ user })
+
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ message: 'Acesso negado!' })
+    }
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: 'Token inválido!' })
+    }
+}
 
 /* Register User */
-app.post('/auth/register', async(req, res) => {
+app.post('/usuario/cadastro', async(req, res) => {
 
     const { name, email, password1, password2 } = req.body
 
     // Validations
     if (!name || !email || !password1 || !password2) {
-        return res.status(422).json({ message: "Todos os dados devem ser preenchidos!" })
+        return res.status(422).json({ message: 'Todos os dados devem ser preenchidos!' })
     }
 
     if (password1 !== password2) {
-        return res.status(422).json({ message: "As senhas devem ser iguais..." })
+        return res.status(422).json({ message: 'As senhas devem ser iguais...' })
     }
 
     // Check if User exists
     const userExists = await User.findOne({ email: email })
 
     if (userExists) {
-        return res.status(422).json({ message: "Usuário já cadastrado no sistema! Por favor utilize outro e-mail..." })
+        return res.status(422).json({ message: 'Usuário já cadastrado no sistema! Por favor utilize outro e-mail...' })
     }
 
     // Create a encrypted password
@@ -63,33 +94,33 @@ app.post('/auth/register', async(req, res) => {
     }
 
     // return res.status(200).json(req.body)
-    // return res.status(200).json({ message: "Dados recebidos: " + JSON.stringify(req.body) })
+    // return res.status(200).json({ message: 'Dados recebidos: ' + JSON.stringify(req.body) })
     // return res.status(200).json({
     //     message: `Usuário cadastrado com sucesso: ${req.body.name}`
     // })
 })
 
 /* Login user */
-app.post('/auth/login', async(req, res) => {
+app.post('/usuario/login', async(req, res) => {
     const { email, password } = req.body
 
     // Validations
     if (!email || !password) {
-        return res.status(422).json({ message: "O E-mail e a senha devem ser preenchidos!" })
+        return res.status(422).json({ message: 'O E-mail e a senha devem ser preenchidos!' })
     }
 
     // Check if User exists
     const user = await User.findOne({ email: email })
 
     if (!user) {
-        return res.status(404).json({ message: "Usuário não cadastrado no sistema! Informe outro e-mail..." })
+        return res.status(404).json({ message: 'Usuário não cadastrado no sistema! Informe outro e-mail...' })
     }
 
     // Check if user password match with stored on database
     const checkPassword = await bcrypt.compare(password, user.password)
 
     if (!checkPassword) {
-        return res.status(401).json({ message: "A senha digitada não combina com a senha do usuário..." })
+        return res.status(401).json({ message: 'A senha digitada não combina com a senha do usuário...' })
     }
 
     // Formatting a TOKEN to user
@@ -100,16 +131,14 @@ app.post('/auth/login', async(req, res) => {
 
         }, secret)
 
-        console.log(token)
-
-        return res.status(200).json({ message: "Usuário logado com sucesso!" })
+        return res.status(200).json({ message: 'Autenticação realizada com sucesso!', token })
 
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: 'Aconteceu um erro no servidor! Tente novamente mais tarde...' })
     }
 
-    // return res.status(200).json({ message: "Validações ok!!!" })
+    // return res.status(200).json({ message: 'Validações ok!!!' })
 
 })
 
